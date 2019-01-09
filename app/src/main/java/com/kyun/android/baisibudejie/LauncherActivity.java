72fd726583adebcf8f95a7ecd9bf103f55ad034c
caiwubin <84606895@qq.com>
2019-01-09 16:13:33 +0800

    complete launcher activity

---
+++ app/src/main/java/com/kyun/android/baisibudejie/LauncherActivity.java
@@ -0,0 +1,31 @@
+package com.kyun.android.baisibudejie;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        View target = findViewById(R.id.iv_bg);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(target, "alpha",0.0f, 1.0f);
+        objectAnimator.setDuration(2000);
+        objectAnimator.start();
+        objectAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                startActivity(new Intent(LauncherActivity.this,MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
